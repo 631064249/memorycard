3278ebe13c123322a903935256d8b3e6b3bb6112
yanweicode <yanweicode@gmail.com>
2015-07-04 11:34:48 +0800

    删除不必要的文件

--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\bin\AndroidManifest.xml : \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\AndroidManifest.xml \
--- proj.android/bin/classes/R.java.d
+++
@@ -1,9 +0,0 @@
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\gen\org\cocos\CocosMemoryCard\R.java \
- : D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\drawable-hdpi\ic.png \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\drawable-hdpi\icon.png \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\drawable-ldpi\ic.png \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\drawable-ldpi\icon.png \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\drawable-mdpi\ic.png \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\drawable-mdpi\icon.png \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\res\values\strings.xml \
-D:\cocosDocument\Cocos\CocosProjects\CocosMemoryCard\proj.android\bin\AndroidManifest.xml \
